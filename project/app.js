bbbac1b2556575a5301137ba3ca2e44b8553bf24
lg <whomsp@gmail.com>
2023-05-27 21:38:45 +0900

    edited door page

--- project/app.js
+++ project/app.js
@@ -37,9 +37,34 @@ const threeRandom = document.querySelector('.random .three');
 const fourRandom = document.querySelector('.random .four');
 const fiveRandom = document.querySelector('.random .five');
 
+let clickedNumber = () => {
+	console.log(document.querySelector('div').textContent);
+	return document.querySelector('.random').textContent;
+};
+const callWords = () => {
+	fetch(
+		`https://jlpt-vocab-api.vercel.app/api/words/random?level=${clickedNumber}`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			console.log(data.furigana, data.word);
+			// const word = document.createElement('div');
+			// word.textContent = data.word;
+			// document.body.appendChild(word);
+			modal.innerHTML = `
+      ${data.level}급 단어
+      히라가나 : ${data.furigana}  
+      한자 : ${data.word}
+      뜻 : ${data.meaning}
+      발음 : ${data.romaji}`;
+		});
+};
+
 oneRandom.addEventListener('click', () => {
 	modal.style.display = 'block';
 	document.body.style.backgroundColor = '#f3f3f3';
+	callWords();
 });
 
 twoRandom.addEventListener('click', () => {
@@ -53,4 +78,16 @@ fourRandom.addEventListener('click', () => {
 });
 fiveRandom.addEventListener('click', () => {
 	console.log('five click');
+});
+
+//-----
+const walking = document.querySelector('.walking');
+walking.addEventListener('click', () => {
+	console.log('22222');
+});
+
+const text = document.querySelector('.bubble');
+
+text.addEventListener('click', () => {
+	console.log('five sssss');
 });
