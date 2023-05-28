8dd7bbc7de309406f200bc8b6b8d41336278808e
lg <whomsp@gmail.com>
2023-05-28 22:47:18 +0900

    editing main page

--- project/app.js
+++ project/app.js
@@ -1,3 +1,4 @@
+// navbar toggle button
 const toggleButton = document.querySelector('.navbar-toggle');
 const menu = document.querySelector('.navbar-menu');
 const side = document.querySelector('.navbar-side');
@@ -17,14 +18,9 @@ toggleButton.addEventListener('click', () => {
 // 	side.classList.toggle('active');
 // });
 
-// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ/
-// const clickLevel = document.querySelectorAll('.click');
-
+// modal
 const modal = document.querySelector('.modal');
 
-// clickLevel.addEventListener('click', () => {
-// 	modal.style.display = 'block';
-// });
 const oneRandom = document.querySelector('.random .one');
 const twoRandom = document.querySelector('.random .two');
 const threeRandom = document.querySelector('.random .three');
@@ -33,47 +29,69 @@ const fiveRandom = document.querySelector('.random .five');
 
 const main = document.querySelector('.main');
 
+const p = document.querySelector('.modal p');
+
 let clickedNumber = () => {
-	console.log(document.querySelector('div').textContent);
-	return document.querySelector('.random').textContent;
+	console.log(document.querySelector('.click').innerText);
+	return document.querySelector('.click').innerText;
 };
+
+// call words api
 const callWords = () => {
 	fetch(
 		`https://jlpt-vocab-api.vercel.app/api/words/random?level=${clickedNumber}`
 	)
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
 			console.log(data.furigana, data.word);
-			// const word =   document.createElement('div');
-			// word.textContent = data.word;
-			// document.body.appendChild(word);
-			modal.innerHTML = `
-      ${data.level}급 단어
-      히라가나 : ${data.furigana}  
-      한자 : ${data.word}
-      뜻 : ${data.meaning}
-      발음 : ${data.romaji}`;
+			let level = document.createElement('h2');
+			let p = document.createElement('p');
+			let button = document.createElement('button');
+			modal.appendChild(level).innerText = `[ ${data.level}급 단어 ]`;
+			modal.appendChild(p).innerText = `
+      ✍
+      
+			◾ 히라가나 : ${data.furigana}
+			  ◽ 한자 : ${data.word}
+			  ◾ 뜻 : ${data.meaning}
+			  ◽ 발음 : ${data.romaji}`;
+			modal.appendChild(button).innerText = '❌';
 		});
 };
 
+// go to main button
+// const backBtn = document.querySelector('.modal');
+// backBtn.addEventListener('click', () => {
+// 	modal.style.display = 'none';
+// 	main.style.display = 'block';
+// });
+
+// each level box 1 ~ 5
 oneRandom.addEventListener('click', () => {
 	modal.style.display = 'block';
-	main.innerHTML = '';
+	main.style.display = 'none';
 	callWords();
 });
 
 twoRandom.addEventListener('click', () => {
-	console.log('two click');
+	modal.style.display = 'block';
+	main.style.display = 'none';
+	callWords();
 });
 threeRandom.addEventListener('click', () => {
-	console.log('three click');
+	modal.style.display = 'block';
+	main.style.display = 'none';
+	callWords();
 });
 fourRandom.addEventListener('click', () => {
-	console.log('four click');
+	modal.style.display = 'block';
+	main.style.display = 'none';
+	callWords();
 });
 fiveRandom.addEventListener('click', () => {
-	console.log('five click');
+	modal.style.display = 'block';
+	main.style.display = 'none';
+	callWords();
 });
 
 //-----
@@ -91,5 +109,11 @@ text.addEventListener('click', () => {
 //-----
 const darkmode = document.querySelector('.darkmode');
 darkmode.addEventListener('click', () => {
+	darkmode.style.color = 'red';
 	console.log('222222222222222222222222222222222');
+});
+
+const heading = document.querySelector('.heading');
+heading.addEventListener('click', () => {
+	console.log('head');
 });
