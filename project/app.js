a4f48549703c65e5614db75be20caf83447b3099
lg <whomsp@gmail.com>
2023-05-29 16:29:40 +0900

    editing main page

--- project/app.js
+++ project/app.js
@@ -1,4 +1,4 @@
-// navbar toggle button
+// navbar toggle button -----------------------------------------
 const toggleButton = document.querySelector('.navbar-toggle');
 const menu = document.querySelector('.navbar-menu');
 const side = document.querySelector('.navbar-side');
@@ -18,7 +18,7 @@ toggleButton.addEventListener('click', () => {
 // 	side.classList.toggle('active');
 // });
 
-// modal
+// modal --------------------------------------------------------
 const modal = document.querySelector('.modal');
 
 const oneRandom = document.querySelector('.random .one');
@@ -28,45 +28,50 @@ const fourRandom = document.querySelector('.random .four');
 const fiveRandom = document.querySelector('.random .five');
 
 const main = document.querySelector('.main');
-
 const p = document.querySelector('.modal p');
 
-let clickedNumber = () => {
-	console.log(document.querySelector('.click').innerText);
-	return document.querySelector('.click').innerText;
-};
+let clickedNumber = document.querySelector('.level-box');
+clickedNumber.addEventListener('click', (e) => {
+	clickedNumber = e.target.innerText;
+	console.log(clickedNumber.value);
+});
 
-// call words api
+// call words api ------------------------------------------------
 const callWords = () => {
 	fetch(
 		`https://jlpt-vocab-api.vercel.app/api/words/random?level=${clickedNumber}`
 	)
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data.furigana, data.word);
+			modal.innerHTML = '';
 			let level = document.createElement('h2');
 			let p = document.createElement('p');
 			let button = document.createElement('button');
+			let check = document.createElement('input');
+
 			modal.appendChild(level).innerText = `[ ${data.level}급 단어 ]`;
 			modal.appendChild(p).innerText = `
       ✍
       
 			◾ 히라가나 : ${data.furigana}
-			  ◽ 한자 : ${data.word}
-			  ◾ 뜻 : ${data.meaning}
-			  ◽ 발음 : ${data.romaji}`;
-			modal.appendChild(button).innerText = '❌';
+      ◽ 한자 : ${data.word}
+      ◾ 발음 : ${data.romaji}
+      ◽ 뜻 : ${data.meaning}
+
+      ✔ 암기완료?
+      👇 체크!`;
+			modal.appendChild(check).type = 'checkbox';
+			modal.appendChild(button).innerText = '닫기';
+			button.classList.add('button');
+			const backBtn = document.querySelector('.button');
+			backBtn.addEventListener('click', () => {
+				modal.style.display = 'none';
+				main.style.display = 'block';
+			});
 		});
 };
 
-// go to main button
-// const backBtn = document.querySelector('.modal');
-// backBtn.addEventListener('click', () => {
-// 	modal.style.display = 'none';
-// 	main.style.display = 'block';
-// });
-
-// each level box 1 ~ 5
+// each level box 1 ~ 5 --------------------------------------
 oneRandom.addEventListener('click', () => {
 	modal.style.display = 'block';
 	main.style.display = 'none';
@@ -94,17 +99,69 @@ fiveRandom.addEventListener('click', () => {
 	callWords();
 });
 
+// call all words -----------------------------------------------
+const callAllWords = () => {
+	const time = document.createElement('h1');
+	modal.appendChild(time).innerText = '⏱단어를 불러오는 중입니다!⏱';
+	setTimeout(() => modal.removeChild(time), 1200);
+
+	fetch(`https://jlpt-vocab-api.vercel.app/api/words/all?level=${1}`)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data.map((word) => word));
+			console.log(data.map((w) => w)[0]);
+			const num = document.createElement('h2');
+			const p = document.createElement('p');
+			const button = document.createElement('button');
+			modal.appendChild(button).innerText = 'back';
+			modal.appendChild(
+				num
+			).innerText = `총 ${data.length}개의 단어를 불러왔습니다.`;
+			modal.appendChild(p).innerText = data.map(
+				(word) => `
+      🔸 히라가나 : ${word.furigana}
+      🔹 한자 : ${word.word}
+      🔸 발음 : ${word.romaji}
+      🔹 뜻 : ${word.meaning}
+
+      🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵🔵
+      
+      `
+			);
+			button.addEventListener('click', () => {
+				modal.style.display = 'none';
+				main.style.display = 'block';
+			});
+		});
+	modal.innerHTML = null;
+	const time1 = document.createElement('h1');
+	modal.appendChild(time1).innerText = '⏱단어를 불러오는 중입니다!⏱';
+	setTimeout(() => modal.removeChild(time1), 1200);
+};
+
+const oneAll = document.querySelector('.all .one');
+const twoAll = document.querySelector('.all .two');
+const threeAll = document.querySelector('.all .three');
+const fourAll = document.querySelector('.all .four');
+const fiveAll = document.querySelector('.all .five');
+
+oneAll.addEventListener('click', () => {
+	modal.style.display = 'block';
+	main.style.display = 'none';
+	callAllWords();
+});
+
 //-----
-const walking = document.querySelector('.walking');
-walking.addEventListener('click', () => {
-	console.log('22222');
-});
+// const walking = document.querySelector('.walking');
+// walking.addEventListener('click', () => {
+// 	console.log('22222');
+// });
 
-const text = document.querySelector('.bubble');
+// const text = document.querySelector('.bubble');
 
-text.addEventListener('click', () => {
-	console.log('five sssss');
-});
+// text.addEventListener('click', () => {
+// 	console.log('five sssss');
+// });
 
 //-----
 const darkmode = document.querySelector('.darkmode');
